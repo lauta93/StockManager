ce36fb18f9b8cf54735074ecceb0447f9d02f47d
¨lauta93¨ <lauta..boehler@gmail.com>
2025-10-28 19:55:34 -0300

    ´´

---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿namespace StockManager.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        
+        //Clave foranea de la categoria padre
+        public int? ParentCategoryId { get; set; }
+        //Crea la relacion de auto referencia
+        public Category? ParentCategory { get; set; }
+        //Crea la relacion (1:N) en la BD, una categoria puede tener muchas subcategorias de tipo Category
+        public ICollection<Category> SubCategories { get; set; } = new List<Category>();
+        //Crea la relacion con los productos (1:N), una categoria puede tener muchos productos
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace StockManager.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int MinimumStock { get; set; }
+        //Clave foranea de la categoria
+        public int CategoryId { get; set; }
+        //Crea la relacion con la categoria (N:1), muchos productos pueden pertenecer a una categoria
+        public Category? Category { get; set; }
+    }
+}
